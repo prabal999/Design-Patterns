dc8466fb8a013870ba66d1534253793e0ded3dd8
Prabal Srivastava <prabal9@live.com>
2018-12-09 13:38:22 +0530

    Behavioral | Interpreter

---
+++ src/main/java/com/prabalhub/design/patterns/behavioral/interpreter/Expression.java
@@ -0,0 +1,5 @@
+package com.prabalhub.design.patterns.behavioral.interpreter;
+
+public interface Expression {
+	boolean interpreter(String context); 
+}
---
+++ src/main/java/com/prabalhub/design/patterns/behavioral/interpreter/TerminalExpression.java
@@ -0,0 +1,14 @@
+package com.prabalhub.design.patterns.behavioral.interpreter;
+
+public class TerminalExpression implements Expression {
+	private String data;
+	public TerminalExpression(String data) {
+		this.data = data;
+	}
+	
+	@Override
+	public boolean interpreter(String context) {
+		return context.contains(data);
+	}
+
+}
